5ac0363325a0a533bfe359b6741e25a366dbfc7b
kateliberko <kliberko@macalester.edu>
2021-11-28 21:47:35 -0600

    working login/signup forms (no database yet)

--- JournalProject/app.py
+++ JournalProject/app.py
@@ -1,7 +1,10 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import SignUpForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='9f7615199c57c5ce0b76e926041133b3'
 
 @app.route("/") 
 def mainpage():
@@ -26,3 +29,23 @@ def moodtracker():
 @app.route("/info") 
 def info():
     return render_template("info.html")
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('mainpage'))
+    return render_template('signup.html', title='Sign Up', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('mainpage'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
